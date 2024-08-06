bd72d7c929ca968b757f9c9adbfa2b426da93b4d
apolinex <jose.teixeira@abo.fi>
2024-08-06 21:34:42 +0300

    added links

--- test-examples/paper-to-Wiley-Blackwell-journal/latextoolsforwritingacademicpapers/bin/assume-unchanged-all-docs.sh
+++
@@ -1,4 +0,0 @@
-echo "updating git index to assume all pdf-files in this folder as unchanged"
-git update-index --verbose --assume-unchange *.docx
-git update-index --verbose --assume-unchange *.doc
-echo "DONE"
--- test-examples/paper-to-Wiley-Blackwell-journal/latextoolsforwritingacademicpapers/bin/assume-unchanged-all-pdf.sh
+++
@@ -1,3 +0,0 @@
-echo "updating git index to assume all pdf-files in this folder as unchanged"
-git update-index --verbose --assume-unchange *.pdf
-echo "DONE"
